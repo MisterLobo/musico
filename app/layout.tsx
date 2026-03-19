ea110c7b38e2e00714d005c6692b588f906fa003
MisterLobo <algae1234567890@gmail.com>
2026-03-19 14:02:50 +0800

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: 'musico',
+  description: 'Book and practice music at studios near you',
 };
 
 const geistSans = Geist({
