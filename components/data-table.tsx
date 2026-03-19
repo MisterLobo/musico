0eeecc41c16f227ae6385907deaf73e954a582e8
MisterLobo <algae1234567890@gmail.com>
2026-03-19 13:37:55 +0800

    removed "variant" attr

--- components/data-table.tsx
+++ components/data-table.tsx
@@ -304,7 +304,7 @@ const columns: ColumnDef<z.infer<typeof schema>>[] = [
           <DropdownMenuItem>Make a copy</DropdownMenuItem>
           <DropdownMenuItem>Favorite</DropdownMenuItem>
           <DropdownMenuSeparator />
-          <DropdownMenuItem variant="destructive">Delete</DropdownMenuItem>
+          <DropdownMenuItem>Delete</DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
     ),
